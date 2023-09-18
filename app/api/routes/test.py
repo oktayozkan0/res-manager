357329e4f65bab0dc9d0701322a5ba1b59afb0a9
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2023-09-18 16:04:13 +0300

    test routes created

---
+++ app/api/routes/test.py
@@ -0,0 +1,31 @@
+from starlette.requests import Request
+from fastapi import APIRouter, Depends
+from app.db.database import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.test import TestCreateSchema, TestUpdateSchema
+from app.db.models.test import Test
+from app.services.test import get_test_service, create_test_service, update_test_service
+
+
+router = APIRouter()
+
+@router.post("/icreate")
+async def create_test(
+    payload: TestCreateSchema,
+    db: AsyncSession = Depends(get_db)
+):
+    return await create_test_service(db, payload)
+
+@router.get("/iget")
+async def get_test(
+    request: Request,
+    db: AsyncSession = Depends(get_db)
+):
+    return await get_test_service(db=db)
+
+@router.post("/iupdate")
+async def update_test(
+    payload: TestUpdateSchema,
+    db: AsyncSession = Depends(get_db)
+):
+    return await update_test_service(db=db, payload=payload)
