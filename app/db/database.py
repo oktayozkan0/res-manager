c4df1fa37b12d0d191b17de99c3890aa8cc2a130
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2023-09-18 15:48:56 +0300

    database connection string configured

--- app/db/database.py
+++ app/db/database.py
@@ -3,9 +3,12 @@ from collections.abc import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
+from app.core.settings.app import AppSettings
+
+settings = AppSettings()
 
 engine = create_async_engine(
-    "postgresql+asyncpg://postgres:postgres@localhost/test",
+    settings.database_url.unicode_string(),
     future=True,
     echo=True,
 )
